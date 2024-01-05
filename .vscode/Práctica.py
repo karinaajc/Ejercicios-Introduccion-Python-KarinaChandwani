a1e4cba6b08c849a653eecb581f8799b69ae09de
karinaajc <karinaa_795@hotmail.com>
2024-01-05 18:13:54 +0100

    Create Práctica.py

---
+++ .vscode/Práctica.py
@@ -0,0 +1,22 @@
+## Crea una variable llamada conejos y asígnale un valor de 126
+conejos = 126
+
+## Crea una variable llamada zanahorias y asígnale valor 0 
+zanahorias = 0
+
+## Muestra el contenido de variables conejos 
+print(conejos)
+
+## Modifica el valor de la variable conejos por 150
+conejos = 150
+
+## Copia el valor de la variable conejos en la variable zanahorias
+zanahorias=conejos
+
+## Imprime el valor de las dos variables con print()
+print(conejos,zanahorias)
+
+## Modifica el valor de conejos por 250 y vuelve a mostrar las dos variables
+conejos = 250
+print(conejos,zanahorias)
+
